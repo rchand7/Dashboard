bad84707faea4edf9ca53c7c99b7314d2b2b18cf
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-02-07 11:46:41 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import pandas as pd 
 import os 
 import warnings 
+import matplotlib
 warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:", layout="wide")
@@ -16,11 +17,8 @@ if fl is not None:
     # If a file is uploaded, read that file
     filename = fl.name
     st.write(filename)
-    df = pd.read_csv(fl, encoding="ISO-8859-1")
-else:
-    # If no file is uploaded, read the default file from the specified directory
-    default_file_path = r"C:\Users\chand\OneDrive\Desktop\Dashboard\dashboard.py"
-    df = pd.read_csv(default_file_path, encoding="ISO-8859-1")
+
+    df = pd.read_csv("Superstore.csv", encoding="ISO-8859-1")
 
     col1, col2=st.columns((2))
     df["Order Date"]= pd.to_datetime(df["Order Date"])
